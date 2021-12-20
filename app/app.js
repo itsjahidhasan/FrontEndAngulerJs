4bb25073db3158ccc96d2160e66c262e9eca5867
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-12-20 13:25:29 +0600

    Admin frontEnd

---
+++ app/app.js
@@ -0,0 +1,71 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(["$routeProvider","$locationProvider",function($routeProvider,$locationProvider){
+
+  $routeProvider
+
+  //public route
+  .when("/",{
+    templateUrl:"views/pages/home.html",
+    // controller:''
+  })
+  .when("/Products",{
+    templateUrl:"views/pages/products.html",
+    // controller:''
+  })
+  .when("/Shoplists",{
+    templateUrl:"views/pages/shoplists.html",
+    // controller:''
+  })
+  .when("/Contact",{
+    templateUrl:"views/pages/contact.html",
+    // controller:''
+  })
+
+
+  // Admin route
+  .when("/Admin",{
+    templateUrl:"views/pages/admin/login.html",
+    controller:'login'
+  })
+  .when("/Admin/Home",{
+    templateUrl:"views/pages/admin/home.html",
+    // controller:''
+  })
+  .when("/Admin/Profile",{
+    templateUrl:"views/pages/admin/profile.html",
+    controller:'profile'
+  })
+  .when("/Admin/User/Info",{
+    templateUrl:"views/pages/admin/userinfo.html",
+    // controller:''
+  })
+  .when("/Admin/Notice",{
+    templateUrl:"views/pages/admin/notice.html",
+    // controller:''
+  })
+
+
+
+  // Shop owner route
+  .when("/ShopownerLogin",{
+    //templateUrl:"",
+    // controller:''
+  })
+
+
+
+  //All logout
+  .when("/Logout",{
+    templateUrl:"views/pages/home.html",
+    controller:'logout'
+  })
+ // All other case
+  .otherwise({
+    redirectTo:"/"
+});
+
+  
+}]);
+app.config(function($httpProvider){
+  $httpProvider.interceptors.push('interCeptor');
+})
